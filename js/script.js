eb25cdf1d46f2e5e3cd55a4f56596ac8fca4777d
Mirko Guerra <mirkoguerra93@gmail.com>
2020-10-23 14:12:43 +0200

    Parte relativa agli input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // parte dedicata alla gestione degli input
+var surnameArray = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi']; // inizializzo un array che contiene un elenco di cognomi non ordinato
+var newSurname = prompt('Inserire il proprio cognome'); // all'utente, in avvio, verrà chiesto di inserire il proprio cognome, che sarà salvato nella variabile newSurname
 
 // parte dedicata alla gestione della logica
+
+newSurname.push(surnameArray); // il cognome inserito dall'utente viene aggiunto ai cognomi non ordinati già presenti all'interno dell'array
+  constructor() {
+
+  }
+}
 
 // parte dedicata alla gestione degli output
